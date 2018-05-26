b950d7931bd8776e568d5296f098a23b695e4bdf
Jason Chen <jason@jcndrop.com>
2018-05-26 16:30:31 -0700

    Modify client to work with redirections

--- src/client.rs
+++ src/client.rs
@@ -1,20 +1,29 @@
 use std::io::Cursor;
 use std::marker::PhantomData;
+use std::sync::Arc;
 
 use api::*;
 use error::Error;
 use resources::TVShow;
 
 use futures::{future, Async, Future, Poll, Stream};
-use hyper::{self, Request};
+use hyper::{self, Request, Response};
 use hyper::client::HttpConnector;
 use hyper::body::Body;
 use serde::de::DeserializeOwned;
 use serde_json;
 use url::form_urlencoded::Serializer;
 
+pub enum QueryState {
+    AwaitingRedirect {
+        http_client: Arc<hyper::Client<HttpConnector, Body>>,
+        inner: Box<Future<Item=Response<Body>, Error=hyper::Error>>,
+    },
+    AwaitingData(Box<Future<Item=Vec<u8>, Error=Error>>),
+}
+
 pub struct QueryFuture<R> {
-    inner: Box<Future<Item=Vec<u8>, Error=hyper::Error>>,
+    state: QueryState,
     _marker: PhantomData<R>,
 }
 
@@ -25,31 +34,67 @@ impl<R: DeserializeOwned> Future for QueryFuture<R> {
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let bytes = try_ready!(self.inner.poll());
-        let cursor = Cursor::new(bytes);
-        let deserialized: R = serde_json::from_reader(cursor)?;
-        Ok(Async::Ready(deserialized))
+        loop {
+            let state = match self.state {
+                QueryState::AwaitingRedirect { ref http_client, ref mut inner } => {
+                    let res = try_ready!(inner.poll());
+                    match res.status().as_u16() {
+                        301 | 302 => {
+                            let url = res.headers().get("Location")
+                                .map(|loc| loc.to_str().map_err(Error::from))
+                                .ok_or(Error::MissingLocationHeader);
+
+                            match url {
+                                Ok(Ok(url)) => {
+                                    let req = Request::get(url).body(Body::empty())?;
+                                    let inner = http_client.request(req)
+                                        .and_then(|res| res.into_body().concat2())
+                                        .map(|chunk| chunk.to_vec())
+                                        .map_err(Error::from);
+
+                                    QueryState::AwaitingData(Box::new(inner))
+                                }
+                                Ok(Err(e)) => { return Err(e); }
+                                Err(e) => { return Err(e); }
+                            }
+                        }
+                        404 => { return Err(Error::ShowNotFound); },
+                        _ => { return Err(Error::UnexpectedResponse); },
+                    }
+                }
+
+                QueryState::AwaitingData(ref mut fut) => {
+                    let bytes = try_ready!(fut.poll());
+                    let cursor = Cursor::new(bytes);
+                    let deserialized: R = serde_json::from_reader(cursor)?;
+                    return Ok(Async::Ready(deserialized));
+                }
+            };
+
+            self.state = state;
+        }
     }
 }
 
 pub struct Client {
-    http_client: hyper::Client<HttpConnector, Body>,
+    http_client: Arc<hyper::Client<HttpConnector, Body>>,
 }
 
 impl Client {
     pub fn new() -> Client {
         Client {
-            http_client: hyper::Client::new(),
+            http_client: Arc::new(hyper::Client::new()),
         }
     }
 
     fn create_future<R: DeserializeOwned>(&self, req: Request<Body>) -> QueryFuture<R> {
         let inner = self.http_client.request(req)
             .and_then(|res| res.into_body().concat2())
-            .map(|chunk| chunk.to_vec());
+            .map(|chunk| chunk.to_vec())
+            .map_err(Error::from);
 
         QueryFuture {
-            inner: Box::new(inner),
+            state: QueryState::AwaitingData(Box::new(inner)),
             _marker: PhantomData,
         }
     }
